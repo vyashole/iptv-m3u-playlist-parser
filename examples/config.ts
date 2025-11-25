2bdbbd85dca363fcb9aabd5dc0aaeff902483e30
Adwait Vyas <26061202+vyashole@users.noreply.github.com>
2025-11-25 12:02:13 +0100

    config.ts update

--- examples/config.ts
+++ examples/config.ts
@@ -1,13 +1,14 @@
 import { HttpsProxyAgent } from "https-proxy-agent";
 
-const PROXY_URL = "http://147.75.88.120:9400";
+const PROXY_URL = "https://8.212.168.170:808";
 
 const agent = new HttpsProxyAgent(PROXY_URL);
 
 
 
 export const SOURCES = [
-//    {hostname: "hilay.tv", path: "/play.m3u", agent },
+    {hostname: "hilay.tv", path: "/play.m3u", agent },
+    'https://raw.githubusercontent.com/abusaeeidx/IPTV-Scraper-Zilla/refs/heads/main/CricHD.m3u',
     'https://hilay.tv/play.m3u',
     'https://raw.githubusercontent.com/abusaeeidx/IPTV-Scraper-Zilla/main/combined-playlist.m3u',
     'https://iptv-org.github.io/iptv/categories/sports.m3u',
